1f5395c4a31ece243ee292f749dc095b449ad7ff
SageTendo <39864316+SageTendo@users.noreply.github.com>
2026-01-20 23:22:41 +0200

    feat: Implemented authentication

--- kitsu/client.py
+++ kitsu/client.py
@@ -23,6 +23,7 @@ SOFTWARE.
 """
 from __future__ import annotations
 
+from datetime import datetime, timedelta
 import logging
 from typing import Any, List, Optional, Union
 
@@ -35,16 +36,60 @@ __all__ = ("Client",)
 __log__: logging.Logger = logging.getLogger(__name__)
 
 BASE: str = "https://kitsu.io/api/edge"
+AUTH_BASE: str = "https://kitsu.io/api/oauth"
 
 
 class Client:
     """User client used to interact with the Kitsu API"""
 
-    def __init__(self, session: Optional[aiohttp.ClientSession] = None) -> None:
+    def __init__(
+        self,
+        session: Optional[aiohttp.ClientSession] = None,
+        client_id: Optional[str] = None,
+        client_secret: Optional[str] = None,
+    ) -> None:
         self._session: aiohttp.ClientSession = session or aiohttp.ClientSession()
+        self._CLIENT_ID: Optional[str] = client_id
+        self._CLIENT_SECRET: Optional[str] = client_secret
+
+        self._token: Optional[str] = None
+        self._refresh_token: Optional[str] = None
+        self._token_expires: Optional[datetime] = None
 
     def __repr__(self) -> str:
         return "<kitsu.Client>"
+
+    def __add_client_info(self, data: dict) -> dict:
+        if self._CLIENT_ID and self._CLIENT_SECRET:
+            data["client_id"] = self._CLIENT_ID
+            data["client_secret"] = self._CLIENT_SECRET
+        return data
+
+    async def _post(self, url: str, **kwargs: Any) -> Any:
+        """Performs a POST request to the Kitsu API"""
+
+        headers = kwargs.get("headers", {})
+        headers["Accept"] = "application/json"
+        headers["Content-Type"] = "application/json"
+        headers["User-Agent"] = "Kitsu.py (https://github.com/SageTendo/kitsu.py)"
+
+        data = kwargs.get("data", {})
+
+        __log__.debug("Request URL: %s", url)
+        __log__.debug("Request Headers: %s", headers)
+        __log__.debug("Request Body: %s", data)
+
+        async with self._session.post(url=url, headers=headers, json=data) as response:
+            data = await response.json()
+
+            if response.status == 200:
+                return data
+            if response.status == 400:
+                raise BadRequest(response, data["error"])
+            if response.status == 404:
+                raise NotFound(response, data["error"])
+
+            raise HTTPException(response, await response.text(), response.status)
 
     async def _get(self, url: str, **kwargs: Any) -> Any:
         """Performs a GET request to the Kitsu API"""
@@ -53,7 +98,7 @@ class Client:
 
         headers["Accept"] = "application/vnd.api+json"
         headers["Content-Type"] = "application/vnd.api+json"
-        headers["User-Agent"] = "Kitsu.py (https://github.com/MrArkon/kitsu.py)"
+        headers["User-Agent"] = "Kitsu.py (https://github.com/SageTendo/kitsu.py)"
         kwargs["headers"] = headers
 
         __log__.debug("Request Headers: %s", headers)
@@ -71,6 +116,35 @@ class Client:
                 raise NotFound(response, data["errors"][0]["detail"])
 
             raise HTTPException(response, await response.text(), response.status)
+
+    async def authenticate(self, username: str, password: str) -> None:
+        data = self.__add_client_info(
+            {
+                "grant_type": "password",
+                "username": username,
+                "password": password,
+            }
+        )
+        response = await self._post(url=f"{AUTH_BASE}/token", data=data)
+
+        self._token = response["access_token"]
+        self._refresh_token = response["refresh_token"]
+        created_at = datetime.fromtimestamp(response["created_at"])
+        self._token_expires = created_at + timedelta(seconds=response["expires_in"])
+
+    async def refresh_token(self) -> None:
+        headers = {"Authorization": f"Bearer {self._token}"}
+        data = self.__add_client_info(
+            {
+                "grant_type": "refresh_token",
+                "refresh_token": self._refresh_token,
+            }
+        )
+        data = await self._post(url=f"{AUTH_BASE}/token", data=data, headers=headers)
+        self._token = data["access_token"]
+        self._refresh_token = data["refresh_token"]
+        created_at = datetime.fromtimestamp(data["created_at"])
+        self._token_expires = created_at + timedelta(seconds=data["expires_in"])
 
     async def get_anime(self, anime_id: int, *, raw: bool = False) -> Union[Anime, dict]:
         """Get information of an anime by ID"""
